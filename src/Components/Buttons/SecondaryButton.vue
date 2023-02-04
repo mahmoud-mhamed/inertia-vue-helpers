932c11fad73c7ceb30499474542d73a7f1b5a5b0
mahmoud-mhamed <mahmoud1272022@gmail.com>
2023-02-04 13:36:59 +0300

    [FEAT] work in primary ,secondary button

--- src/Components/Buttons/SecondaryButton.vue
+++ src/Components/Buttons/SecondaryButton.vue
@@ -1,6 +1,6 @@
 <template>
     <component :is="href?'Link':'button'"
-               class="bg-white inline-block text-secondary-800 border border-secondary-600 disabled:opacity-80
+               class="bg-primary-500 inline-block text-secondary-800 border border-secondary-600 disabled:opacity-80
     disabled:cursor-progress text-white inline-block py-2 px-8
      rounded hover:bg-secondary-500 hover:text-white transition-all duration-300 text-base font-bold"
             v-bind="$attrs">
