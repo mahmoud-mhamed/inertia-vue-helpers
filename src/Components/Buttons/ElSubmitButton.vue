6526b53117cc98574bfbad3c32a3eff38e631613
mahmoud-mhamed <mahmoud1272022@gmail.com>
2024-07-16 10:30:51 +0300

    [FEAT] update ability directive

--- src/Components/Buttons/ElSubmitButton.vue
+++ src/Components/Buttons/ElSubmitButton.vue
@@ -1,14 +1,14 @@
 <template>
     <button class="bg-primary-600 border border-primary-600 disabled:opacity-80 disabled:cursor-progress text-white py-2 px-8
      rounded hover:bg-primary-500 transition-all duration-300 text-base font-bold" as="button" type="submit" :disabled="progressing">
-        <loading-dots v-if="progressing"/>
+        <el-loading-dots v-if="progressing"/>
         <span v-else v-text="text" />
         <slot v-else/>
     </button>
 </template>
 
 <script setup>
-import LoadingDots from "../LoadingDots.vue";
+import ElLoadingDots from "../ElLoadingDots.vue";
 defineProps({
     progressing: {
         type: Boolean,
